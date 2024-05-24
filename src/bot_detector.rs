063d5c53df756bf80b64f7820623849afc1d12e8
Murat Genc <muttafi@gmail.com>
2024-05-24 18:37:57 +0300

    magic to const

--- src/bot_detector.rs
+++ src/bot_detector.rs
@@ -3,6 +3,13 @@ use hyper::Request;
 use regex::Regex;
 use tracing::info;
 
+const USER_AGENT_LOG: &str = "User-Agent: {}";
+const ALLOWED_USER_AGENT_LOG: &str = "Allowed User-Agent: {}";
+const DENIED_USER_AGENT_LOG: &str = "Denied User-Agent: {}";
+const PATTERN_MATCHED_USER_AGENT_LOG: &str = "Pattern Matched User-Agent: {}";
+const NO_USER_AGENT_LOG: &str = "No User-Agent header present";
+const EMPTY_USER_AGENT_LOG: &str = "Empty User-Agent detected";
+
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct BotDetectorConfig {
     pub allow: Vec<String>,
@@ -14,32 +21,32 @@ pub struct BotDetectorConfig {
 pub fn is_bot_request(req: &Request<hyper::Body>, config: &BotDetectorConfig) -> bool {
     if let Some(user_agent) = req.headers().get(hyper::header::USER_AGENT) {
         let user_agent = user_agent.to_str().unwrap_or_default();
-        info!("User-Agent: {}", user_agent);
+        info!(USER_AGENT_LOG, user_agent);
 
         if config.allow.iter().any(|s| user_agent.contains(s)) {
-            info!("Allowed User-Agent: {}", user_agent);
+            info!(ALLOWED_USER_AGENT_LOG, user_agent);
             return false;
         }
 
         if config.deny.iter().any(|s| user_agent.contains(s)) {
-            info!("Denied User-Agent: {}", user_agent);
+            info!(DENIED_USER_AGENT_LOG, user_agent);
             return true;
         }
 
         for pattern in &config.patterns {
             if let Ok(regex) = Regex::new(pattern) {
                 if regex.is_match(user_agent) {
-                    info!("Pattern Matched User-Agent: {}", user_agent);
+                    info!(PATTERN_MATCHED_USER_AGENT_LOG, user_agent);
                     return true;
                 }
             }
         }
     } else {
-        info!("No User-Agent header present");
+        info!(NO_USER_AGENT_LOG);
     }
 
     if config.empty_user_agent_is_bot && req.headers().get(hyper::header::USER_AGENT).is_none() {
-        info!("Empty User-Agent detected");
+        info!(EMPTY_USER_AGENT_LOG);
         return true;
     }
 
